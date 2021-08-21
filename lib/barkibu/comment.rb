a325051dff53ebcf03215187d1515170e9c76279
Léo <leo@barkibu.com>
2021-08-21 23:46:11 +0200

    Link Github::PullRequest from Barkibu::Comment !

--- lib/barkibu/comment.rb
+++ lib/barkibu/comment.rb
@@ -1,15 +1,16 @@
 module Barkibu
   class Comment < SimpleDelegator
-    PR_REGEX = %r{https://github\.com/.*/pull/(?<number>\d+)}.freeze
+    PR_REGEX = %r{https://github\.com/(?<org>[^/]*)/(?<repo>[^/]*)/pull/(?<number>\d+)}.freeze
     WORK_STARTED_REGEX = /Moved this to Kanban list: .*In Progress/.freeze
 
     def mention_pr?
       !content.match(PR_REGEX).nil?
     end
 
-    def pr_number
-      match = content.match(PR_REGEX)
-      match[:number].to_i unless match.nil?
+    def pull_requests(github_client)
+      content.scan(PR_REGEX).uniq.map do |org, repo, pull_request_number|
+        github_client.pull_request("#{org}/#{repo}", pull_request_number)
+      end
     end
 
     def work_started?
