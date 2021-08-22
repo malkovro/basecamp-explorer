c66c15aa6c6dc7f878f3ecbeab1b18d2a46896f4
Léo <leo@barkibu.com>
2021-08-22 15:54:57 +0200

    Make authentication endpoint work for github and basecamp

--- lib/ui/auth.rb
+++ lib/ui/auth.rb
@@ -1,12 +1,12 @@
-get '/login' do
-  oauth_negotiator = GATE_KEEPER.present(session)
+get '/:service/login' do
+  oauth_negotiator = GATE_KEEPER.present(params['service'], session)
   redirect oauth_negotiator.authorize_url
 end
 
-get '/token' do
-  oauth_negotiator = GATE_KEEPER.let_in(session, params['code'])
+get '/:service/token' do
+  oauth_negotiator = GATE_KEEPER.let_in(session, params['service'], params['code'])
 
-  return redirect '/login' unless oauth_negotiator
+  return redirect "/#{params['service']}/login" unless oauth_negotiator
 
   redirect '/'
 end
