af5e0e356d7ea420ac648be3557ea96aadedff49
Léo <leo@barkibu.com>
2021-08-22 13:03:53 +0200

    Add completed todos accessor to Basecamp::Todolist

--- lib/basecamp/models/todolist.rb
+++ lib/basecamp/models/todolist.rb
@@ -1,7 +1,11 @@
 module Basecamp
   class Todolist < Model
-    def todos
-      fetch_list todos_url, Todo
+    def incomplete_todos
+      fetch_list todos_url, Todo, completed: false
+    end
+
+    def completed_todos
+      fetch_list todos_url, Todo, completed: true
     end
   end
 end
